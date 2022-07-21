5d0b1a8b487840bace0cd19649a77d74ce506368
amaljom <amaljom99@gmail.com>
2022-07-21 15:59:30 +0200

    creato un input nel header per la ricerca del film

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <input type="text" placeholder="cerca un film" v-model="movieToSearch">
+  </div>
+</template>
+
+<script>
+export default {
+    data:function(){
+        return{
+            movieToSearch:''
+            
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
