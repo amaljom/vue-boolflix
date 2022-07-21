9fb04c055304e459dae020f1d93dee9c416babed
amaljom <amaljom99@gmail.com>
2022-07-21 16:04:30 +0200

    installato bootstrap

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <input type="text" placeholder="cerca un film" v-model="movieToSearch">
+  <div class="bg-success">
+    <input class="p-2 m-2 " type="text" placeholder="cerca un film" v-model="movieToSearch">
   </div>
 </template>
 
@@ -8,8 +8,7 @@
 export default {
     data:function(){
         return{
-            movieToSearch:''
-            
+            movieToSearch:'' 
         }
     },
 }
