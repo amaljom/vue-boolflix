a3062b86152e64a039c9f7a797226e4c408507a6
amaljom <amaljom99@gmail.com>
2022-07-21 16:37:36 +0200

    creazione della card per il film che dovra essere cercato

---
+++ src/components/Main.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="mx-auto col-3 bg-danger text-center mt-3 p-4">
+    <h2>titolo</h2>
+    <h2>titolo originale</h2>
+    <h4>lingua</h4>
+    <h4>voto</h4>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
