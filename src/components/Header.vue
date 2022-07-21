e7e1a93b53807d94415a5d37826ea40cb9a730c2
amaljom <amaljom99@gmail.com>
2022-07-21 16:34:01 +0200

    inserito bottone per la ricerca

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="bg-success">
     <input class="p-2 m-2 " type="text" placeholder="cerca un film" v-model="movieToSearch">
+    <div class="btn btn-primary"> submit </div>   
   </div>
+  
 </template>
 
 <script>
@@ -20,3 +22,4 @@ export default {
 
 
 // 66df3909187524734aebd03e217fd826 api key (me lo segno qua per comodità al momentp)
+// https://api.themoviedb.org/3/search/movie?api_key=66df3909187524734aebd03e217fd826&query=dark
